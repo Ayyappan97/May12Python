70ee1b0eff99bf7b6ccddda9e95995a18503b9db
Ayyappanhsr97 <ayyappanhsr97@gmail.com>
2020-05-12 19:58:14 +0530

    Practice Sets, Tuples, Dictionary, List

---
+++ project2.py
@@ -0,0 +1,68 @@
+# create list
+#lisr exercise
+""""
+programs = ['java', 'python', 'nodejs' , 'angular']
+new = ['golang','sql','c']
+print(programs)
+print(type(programs))
+"""
+#append
+"""
+infra = ['aws', 'gct' , 'azure']
+infra.append('devops')
+print(infra)
+"""
+#insert
+"""
+database = ['mongodb','sql','nosql']
+database.insert(1,'postgre')
+print(database)
+"""
+
+#tuples
+'''
+program = ('angular', 'html','css','javascript')
+print(program)
+print(type(program))
+'''
+#set
+'''
+fruits={"mangoe","orange","apple","grape"}
+fruits.add('pineapple')
+fruits.remove('grape')
+print(fruits)
+print(type(fruits))
+'''
+#dictionary
+'''
+detail={
+    'full_name':'ayyappan',
+    'age': '23',
+    'location': 'chennai'
+}
+print(detail)
+'''
+
+#nested dictionary
+'''
+myskills = {
+  "skill1" : {
+    "name" : "java",
+    "year" : 2017
+  },
+  "skill2" : {
+    "name" : "python",
+    "year" : 2018
+  },
+  "skill3" : {
+    "name" : "nodejs",
+    "year" : 2020
+  }
+}
+print(myskills)
+'''
+
+
+
+
+
